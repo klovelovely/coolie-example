6c6d041cdbd60a1577e644ba1262241183942f2a
张云来 <cloudcome@163.com>
2015-05-11 15:01:10 +0800

    coolie example

--- webroot-pro/static/js/app/area.020eb29b524d7ba6.js
+++ webroot-pro/static/js/app/area.020eb29b524d7ba6.js
@@ -1,3 +1,3 @@
-/*coolie 1430125579055*/
+/*coolie@0.14.4 1431327643332*/
 define("0",["1"],function(t,e,n){"use strict";var u=t("1"),i=document.getElementById("output");document.getElementById("input").onkeyup=function(){var t=u.parseFloat(this.value);i.innerHTML=String(u.PI*Math.pow(t,2))}});
 define("1",[],function(t,a,e){"use strict";a.parseFloat=function(t){return t=parseFloat(t),isNaN(t)?0:t},a.PI=Math.PI});
