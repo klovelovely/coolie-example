c5a38a0f7933b5dbf908d9844a24974ec16f19c9
张云来 <cloudcome@163.com>
2015-06-11 17:04:58 +0800

    up coolie

--- webroot-pro/static/js/app/area.e633541fdaad38d5a0a86b24c3ad419c.js
+++ webroot-pro/static/js/app/area.e633541fdaad38d5a0a86b24c3ad419c.js
@@ -1,3 +1,3 @@
-/*coolie@0.17.0 1433513439275*/
+/*coolie@0.20.5*/
 define("0",["1"],function(t,e,n){"use strict";var u=t("1"),i=document.getElementById("output");document.getElementById("input").onkeyup=function(){var t=u.parseFloat(this.value);i.innerHTML=String(u.PI*Math.pow(t,2))}});
 define("1",[],function(t,a,e){"use strict";a.parseFloat=function(t){t=parseFloat(t);return isNaN(t)?0:t};a.PI=Math.PI});
---
+++ webroot-pro/static/js/coolie-config.42582aad60a9a681775db43d84ca78d7.js
@@ -0,0 +1,2 @@
+/*coolie@0.20.5*/
+coolie.config({base:"./app/",debug:!1,cache:!0,version:{"area.js":"e633541fdaad38d5a0a86b24c3ad419c","length.js":"a8a23147cb6678ea2027feb4fad8911e"}}).use();
